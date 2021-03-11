0e7185797e65c066a1a7e6ab613e01c3370d8314
Archer-Fant <1091053002@qq.com>
2021-03-11 15:13:29 +0800

    字符串回文字专题

---
+++ java/src/Stirng/q28/q28.java
@@ -0,0 +1,18 @@
+package Stirng.q28;
+
+/**
+ * @author Created by Fangzj
+ * @data 2021/3/11 13:21
+ **/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return  haystack.indexOf(needle);
+    }
+}
+public class q28 {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        System.out.println(solution.strStr("hello","ll"));
+    }
+}
+
