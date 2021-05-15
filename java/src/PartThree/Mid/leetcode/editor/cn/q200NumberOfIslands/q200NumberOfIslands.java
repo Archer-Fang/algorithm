fc78b0180670734cc09e488209853793e8b8908e
Archer-Fant <1091053002@qq.com>
2021-05-15 21:27:21 +0800

    mid to 211

---
+++ java/src/PartThree/Mid/leetcode/editor/cn/q200NumberOfIslands/q200NumberOfIslands.java
@@ -0,0 +1,37 @@
+package PartThree.Mid.leetcode.editor.cn.q200NumberOfIslands;
+//Java：岛屿数量
+
+public class q200NumberOfIslands{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIslands(char[][] grid) {
+        int ans=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ java/src/PartThree/Mid/leetcode/editor/cn/q201BitwiseAndOfNumbersRange/q201BitwiseAndOfNumbersRange.java
@@ -0,0 +1,23 @@
+package PartThree.Mid.leetcode.editor.cn.q201BitwiseAndOfNumbersRange;
+//Java：数字范围按位与
+
+public class q201BitwiseAndOfNumbersRange{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+        System.out.println(solution.rangeBitwiseAnd(5,7));
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int count=0;
+        while (left!=right){
+            left>>=1;
+            right>>=1;
+            count++;
+        }
+        return left<<count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
