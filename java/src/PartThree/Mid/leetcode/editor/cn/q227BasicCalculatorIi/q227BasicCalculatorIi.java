e5e1c7aec40a72998b25213e5d867fac0d1e2f98
Archer-Fant <1091053002@qq.com>
2021-05-17 17:20:10 +0800

    mid to 230 5ti

---
+++ java/src/PartThree/Mid/leetcode/editor/cn/q227BasicCalculatorIi/q227BasicCalculatorIi.java
@@ -0,0 +1,68 @@
+package PartThree.Mid.leetcode.editor.cn.q227BasicCalculatorIi;
+//Java：基本计算器 II
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class q227BasicCalculatorIi{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+        solution.calculate("1+2*3");
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public int calculate(String s) {
+        s = s.replaceAll(" ", "");
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('+',1);
+        map.put('-',1);
+        map.put('*',2);
+        map.put('/',2);
+
+        Stack<Long> numStack = new Stack<Long>();
+        Stack<Character> optStack = new Stack<Character>();
+        char[] c = s.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if(Character.isDigit(c[i])){
+                int tmp=Integer.parseInt(String.valueOf(c[i]));
+                while ((i+1)<c.length&&Character.isDigit(c[i+1])){
+                    tmp=tmp*10+Integer.parseInt(String.valueOf(c[++i]));
+                }
+                numStack.push((long) tmp);
+            }else {
+                while (!optStack.isEmpty()){
+                    if((map.get(c[i])<=map.get(optStack.peek()))){
+                        Long b=numStack.pop();
+                        Long a=numStack.pop();
+                        char opt=optStack.pop();
+                        numStack.add(cal(a,b,opt));
+                    }else {
+                        break;
+                    }
+                }
+                optStack.add(c[i]);
+            }
+        }
+        while (!optStack.isEmpty()){
+            Long b=numStack.pop();
+            Long a=numStack.pop();
+            char opt=optStack.pop();
+            numStack.add(cal(a,b,opt));
+        }
+        long tmp=numStack.peek();
+        return (int) tmp;
+    }
+    public long cal(Long a,Long b,char opt){
+        if(opt=='+') return a+b;
+        if(opt=='-') return a-b;
+        if(opt=='*') return a*b;
+        if(opt=='/') return a/b;
+        return -1;
+    }
+}
+
+
+//leetcode submit region end(Prohibit modification and deletion)
