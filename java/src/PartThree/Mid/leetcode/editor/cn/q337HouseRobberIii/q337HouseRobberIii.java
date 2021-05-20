ed332e8d129b6bf0f9759275e1bd91a32581605d
Archer-Fant <1091053002@qq.com>
2021-05-20 14:07:28 +0800

    mid to 378

---
+++ java/src/PartThree/Mid/leetcode/editor/cn/q337HouseRobberIii/q337HouseRobberIii.java
@@ -0,0 +1,57 @@
+package PartThree.Mid.leetcode.editor.cn.q337HouseRobberIii;
+//Java：打家劫舍 III
+
+public class q337HouseRobberIii{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+class Solution {
+    public int rob(TreeNode root) {
+//        int[] result=new int[2];
+//        result[0]=Math.max(root.left[0],root.left[1])+Math.max(root.right[0],root.right[1]);
+//        result[1]=root.val+root.left[0]+root.right[0];
+        int[] result=dfs(root);
+        return Math.max(result[0],result[1]);
+    }
+
+    private int[] dfs(TreeNode root) {
+        if(root==null) return new int[2];
+        int[] left=dfs(root.left);
+        int[] right=dfs(root.right);
+        int[] result=new int[2];
+        result[0]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        result[1]=root.val+left[0]+right[0];
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ java/src/PartThree/Mid/leetcode/editor/cn/q343IntegerBreak/q343IntegerBreak.java
@@ -0,0 +1,35 @@
+package PartThree.Mid.leetcode.editor.cn.q343IntegerBreak;
+//Java：整数拆分
+
+public class q343IntegerBreak{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // TO TEST
+        System.out.println(solution.integerBreak(10));
+
+    }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        int r=dfs(n,dp);
+        return r;
+    }
+
+    private int dfs(int k, int[] dp) {
+        if(dp[k]!=0) return dp[k];
+        int mul=0;
+
+        for (int j = 1; j < k; j++) {
+            mul=Math.max(mul,Math.max(j*dfs(k-j,dp),j*(k-j)));
+        }
+        dp[k]=mul;
+        return dp[k];
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
